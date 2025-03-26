dd4075308311f5ab32081945bbceaa31bd0316ed
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-03-25 21:49:29 -0700

    updating poro_spec, gateway_spec

--- spec/gateways/movie_gateway_spec.rb
+++ spec/gateways/movie_gateway_spec.rb
@@ -10,11 +10,48 @@ RSpec.describe MovieGateway do
       expect(response.first).to respond_to(:id, :title, :vote_average)
     end
 
-    it 'handles a failed API response gracefully', :vcr do
+    it 'handles an empty API response gracefully', :vcr do
       allow(MovieGateway).to receive(:connect_to_gateway).and_return([])
-    
+
       response = MovieGateway.get_top_rated
       expect(response).to eq([])
+    end
+
+    it 'handles API errors gracefully', :vcr do
+      allow(Faraday).to receive(:get).and_raise(Faraday::Error::TimeoutError)
+
+      expect { MovieGateway.get_top_rated }.to raise_error(Faraday::Error::TimeoutError)
+    end
+
+    it 'handles unexpected data structures in the API response', :vcr do
+      allow(MovieGateway).to receive(:parse_data).and_return([{"id" => "123", "title" => "Movie"}])
+
+      response = MovieGateway.get_top_rated
+      expect(response.first.id).to eq("123")
+      expect(response.first.title).to eq("Movie")
+    end
+  end
+
+  describe '.search_movies' do
+    it 'returns an array of Movie objects based on search query', :vcr do
+      response = MovieGateway.search_movies("Lord of the Rings")
+
+      expect(response).to be_a(Array)
+      expect(response.first).to be_an(Movie)
+      expect(response.first).to respond_to(:id, :title, :vote_average)
+    end
+
+    it 'returns an empty array for a search query with no results', :vcr do
+      allow(MovieGateway).to receive(:connect_to_gateway).and_return([])
+
+      response = MovieGateway.search_movies("NonExistentMovie")
+      expect(response).to eq([])
+    end
+
+    it 'handles a search API error gracefully', :vcr do
+      allow(Faraday).to receive(:get).and_raise(Faraday::Error::ConnectionFailed)
+
+      expect { MovieGateway.search_movies("Test") }.to raise_error(Faraday::Error::ConnectionFailed)
     end
   end
 end
--- spec/poro/movie_poro_spec.rb
+++ spec/poro/movie_poro_spec.rb
@@ -1,30 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :poro do
-  describe 'Movie_poro' do
-    it 'formats a movie object' do
-      movie_data = {
-        :adult=>false,
-        :backdrop_path=>nil,
-        :genre_ids=>[],
-        :id=>1356991,
-        :original_language=>"xx",
-        :original_title=>"Lord",
-        :overview=>
-        "Moving and changing worlds. They start with eight images and merge, gradually diminishing. In the end, they intertwine and become one.",
-        :popularity=>0.328,
-        :poster_path=>"/nTUYqnPrTr1CccWjGMVTLdduYm.jpg",
-        :release_date=>"2024-10-26",
-        :title=>"Lord",
-        :video=>false,
-        :vote_average=>0.0,
-        :vote_count=>0
-      }
-      movie = Movie.new(movie_data)
+  describe 'Movie PORO' do
+    let(:default_data) { { id: 1356991, title: "Lord", vote_average: 0.0 } }
+    let(:missing_title) { { id: 123, vote_average: 8.5 } }
+    let(:missing_vote) { { id: 456, title: "No Rating Movie" } }
+    let(:missing_id) { { title: "Mystery Movie", vote_average: 7.2 } }
+    let(:nil_values) { { id: nil, title: nil, vote_average: nil } }
+    let(:unexpected_types) { { id: "789", title: 12345, vote_average: "9.9" } }
+    let(:empty_input) { {} }
 
-      expect(movie.id). to eq("1356991")
-      expect(movie.title).to eq("Lord")
-      expect(movie.vote_average).to eq 0.0
+    subject { Movie.new(movie_data) }
+
+    context 'when given valid data' do
+      let(:movie_data) { default_data }
+
+      it 'formats a movie object correctly' do
+        expect(subject.id).to eq("1356991")
+        expect(subject.title).to eq("Lord")
+        expect(subject.vote_average).to eq(0.0)
+      end
+    end
+
+    context 'when title is missing' do
+      let(:movie_data) { missing_title }
+
+      it 'handles missing title' do
+        expect(subject.id).to eq("123")
+        expect(subject.title).to be_nil
+        expect(subject.vote_average).to eq(8.5)
+      end
+    end
+
+    context 'when vote_average is missing' do
+      let(:movie_data) { missing_vote }
+
+      it 'handles missing vote_average' do
+        expect(subject.id).to eq("456")
+        expect(subject.title).to eq("No Rating Movie")
+        expect(subject.vote_average).to be_nil 
+      end
+    end
+
+    context 'when id is missing' do
+      let(:movie_data) { missing_id }
+
+      it 'handles missing id' do
+        expect(subject.id).to eq("") 
+        expect(subject.title).to eq("Mystery Movie")
+        expect(subject.vote_average).to eq(7.2)
+      end
+    end
+
+    context 'when all values are nil' do
+      let(:movie_data) { nil_values }
+
+      it 'handles nil values for attributes' do
+        expect(subject.id).to eq("")
+        expect(subject.title).to be_nil
+        expect(subject.vote_average).to be_nil
+      end
+    end
+
+    context 'when given unexpected data types' do
+      let(:movie_data) { unexpected_types }
+
+      it 'handles unexpected data types' do
+        expect(subject.id).to eq("789")
+        expect(subject.title).to eq(12345) 
+        expect(subject.vote_average).to eq("9.9") 
+      end
+    end
+
+    context 'when given an empty hash' do
+      let(:movie_data) { empty_input }
+
+      it 'handles empty input gracefully' do
+        expect(subject.id).to eq("")
+        expect(subject.title).to be_nil
+        expect(subject.vote_average).to be_nil
+      end
     end
   end
 end
